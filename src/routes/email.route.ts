ec3601961ea9ba1373c161ea6cdb291095372f68
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-09-30 00:01:50 +0530

    test cicd

--- src/routes/email.route.ts
+++ src/routes/email.route.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
   try {
-    res.status(200).json({ message: "Hello world2" });
+    res.status(200).json({ message: "Hello world3" });
   } catch (error) {
     console.error("An error ocurred:", error);
     res.status(500).json(error);
