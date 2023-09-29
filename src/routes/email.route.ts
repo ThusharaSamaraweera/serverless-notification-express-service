c5d1b926d90a7eb0b176760340ea366c7b61def4
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-09-30 01:17:11 +0530

    setup nodemailer

--- src/routes/email.route.ts
+++ src/routes/email.route.ts
@@ -1,10 +1,31 @@
 import { Request, Response, Router } from "express";
+import * as nodemailer from "nodemailer";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
+  const emailInput: EmailInput = req.body;
+
+  console.log(`emailInput: ${JSON.stringify(emailInput)}`);
+  console.log(`process.env.SMTP_USER: ${process.env.SMTP_USER}`);
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.SMTP_USER,
+      clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_OAUTH_REFRESH_TOKEN,
+    },
+  });
   try {
-    res.status(200).json({ message: "Hello world3" });
+    await transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to: emailInput?.to,
+      subject: emailInput?.subject || "No subject",
+      text: emailInput?.message,
+    });
+    res.status(200).json({ message: `Sent the email to ${emailInput?.to}` });
   } catch (error) {
     console.error("An error ocurred:", error);
     res.status(500).json(error);
@@ -12,3 +33,9 @@ router.get("/", async (req: Request, res: Response) => {
 });
 
 export default router;
+
+interface EmailInput {
+  to: string;
+  subject: string;
+  message: string;
+}
